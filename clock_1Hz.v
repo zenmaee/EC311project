9c56cb0b29191635e7e0fcf1b8b9b2b6c0b91885
zenmaee <60183108+zenmaee@users.noreply.github.com>
2022-11-28 11:36:14 -0500

    Create clock_1Hz.v

---
+++ clock_1Hz.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module clock_1Hz(
+    input clk, /* 1Mhz clock */
+    output reg out_clk = 0
+    );
+    reg [31:0]count = 32'd0;
+    
+    always@(posedge clk) begin 
+        if (count == 32'd100000000) begin 
+            out_clk <= ~out_clk;
+            count <= 0;
+        end else begin
+            count = count + 1;
+        end
+    end 
+endmodule
