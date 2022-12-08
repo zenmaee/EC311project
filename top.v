e00320a3dd03da6b9fc9f14e616ef9dfaee21f69
bonab2002 <99371905+bonab2002@users.noreply.github.com>
2022-12-08 18:41:05 -0500

    Update top.v

--- top.v
+++ top.v
@@ -26,9 +26,10 @@ module top(
     input wire[5:0] seconds,
     input wire[5:0] minutes,
     input wire[4:0] hours, 
+    input wire[1:0]mode,
     output wire VGA_HS_O,       // horizontal sync output
     output wire VGA_VS_O,        // vertical sync output
     output wire [3:0] VGA_R
     );
-    top_square t1(CLK, RST_BTN, seconds, minutes, hours, VGA_HS_O, VGA_VS_O, VGA_R, VGA_G, VGA_B);
+    top_square t1(CLK, RST_BTN, 57, 44, 2, 2'b11, VGA_HS_O, VGA_VS_O, VGA_R);
 endmodule
