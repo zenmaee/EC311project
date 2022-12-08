3f977d8d0e050b9d321ec7bc0229d31f89e1ca5c
True! <102108688+tllrllsll@users.noreply.github.com>
2022-12-07 21:59:43 -0500

    Update top.v

--- top.v
+++ top.v
@@ -23,11 +23,12 @@
 module top(
     input wire CLK,
     input wire RST_BTN,
+    input wire[5:0] seconds,
+    input wire[5:0] minutes,
+    input wire[4:0] hours, 
     output wire VGA_HS_O,       // horizontal sync output
     output wire VGA_VS_O,        // vertical sync output
-    output wire [3:0] VGA_R,    // 4-bit VGA red output
-    output wire [3:0] VGA_G,    // 4-bit VGA green output
-    output wire [3:0] VGA_B 
+    output wire [3:0] VGA_R
     );
-    top_square t1(CLK, RST_BTN, 8'b00000000, 8'b00000001, VGA_HS_O, VGA_VS_O, VGA_R, VGA_G, VGA_B);
+    top_square t1(CLK, RST_BTN, seconds, minutes, hours, VGA_HS_O, VGA_VS_O, VGA_R, VGA_G, VGA_B);
 endmodule
